f4b8509a554749163d0b19a84a95e23a7f699b8d
Workaholic <workaholic@example.com>
2026-02-26 15:00:05 +0100

    style(404): update typographic layout and background image

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -6,8 +6,8 @@ const NotFound = () => {
     return (
         <section className="not-found">
             <div className="not-found-container">
-                <h1 className="not-found-title">EWO!</h1>
-                <h2 className="not-found-heading">Maintain down... We are still<br />building.</h2>
+                <h1 className="not-found-title">ewo</h1>
+                <h2 className="not-found-heading">Page Not Found!!</h2>
                 <p className="not-found-text">We are still cooking, check back later.</p>
                 <Link to="/" className="btn not-found-btn">Back to home</Link>
             </div>
